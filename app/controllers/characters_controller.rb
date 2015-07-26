97843418c8af97bbe979176b30b943bacfce1367
mimimalizam <milana.stojadinov@gmail.com>
2015-07-26 18:58:52 +0200

    Added Characters Controller, changed routes.rb

---
+++ app/controllers/characters_controller.rb
@@ -0,0 +1,6 @@
+class CharactersController < ApplicationController
+# There are public, private and protected methods in Ruby,
+# but only public methods can be actions for controllers.v
+  def new
+  end
+end

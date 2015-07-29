60483e986f36230e338e0b903fecf950da2a6984
mimimalizam <milana.stojadinov@gmail.com>
2015-07-29 11:32:00 +0200

    Fixed ability action create

--- app/controllers/abilities_controller.rb
+++ app/controllers/abilities_controller.rb
@@ -1,14 +1,20 @@
 class AbilitiesController < ApplicationController
 	def new
 		@character = Character.find(params[:character_id])
-  		@ability = @character.abilities.build
+  	@ability = @character.abilities.build
   	end
 
   	def create
+      debugger
   		@character = Character.find(params[:character_id])
-  		debugger
-  		@ability = @character.abilities.build(params[:ability])
+  		#debugger
+  		@ability = @character.abilities.build(allowed_params)
   		@ability.save
   		redirect_to character_path(@character)
   	end
+
+    private 
+    def allowed_params
+      params.require(:ability).permit(:name, :level)
+    end
 end

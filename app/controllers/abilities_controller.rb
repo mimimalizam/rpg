70dda9c54ade0266ae69b9283b7142edb83d0918
mimimalizam <milana.stojadinov@gmail.com>
2015-07-29 14:06:32 +0200

    CRUD Ability

--- app/controllers/abilities_controller.rb
+++ app/controllers/abilities_controller.rb
@@ -4,14 +4,36 @@ class AbilitiesController < ApplicationController
   	@ability = @character.abilities.build
   	end
 
-  	def create
-      debugger
-  		@character = Character.find(params[:character_id])
-  		#debugger
-  		@ability = @character.abilities.build(allowed_params)
-  		@ability.save
-  		redirect_to character_path(@character)
-  	end
+  def edit
+    @character = Character.find(params[:character_id])
+    @ability = Ability.find(params[:id])
+  end
+
+  def create
+  	@character = Character.find(params[:character_id])  		
+  	@ability = @character.abilities.build(allowed_params)
+  	@ability.save
+		redirect_to character_path(@character)
+ 	end
+
+  def update
+    @character = Character.find(params[:character_id])
+    @ability = Ability.find(params[:id])
+    if @ability.update(allowed_params)
+      redirect_to @character
+  else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @character = Character.find(params[:character_id])
+    @ability = Ability.find(params[:id])    
+    @ability.destroy
+ 
+    redirect_to @character
+  end
+
 
     private 
     def allowed_params

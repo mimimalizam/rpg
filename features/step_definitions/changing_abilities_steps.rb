7ef49f22ddb7703f5f396c153b347c1887171f6c
Milana Stojadinov <mstojadinov@renderedtext.com>
2015-08-12 13:46:24 +0200

    Reorganised abilities scenarios

--- features/step_definitions/changing_abilities_steps.rb
+++ features/step_definitions/changing_abilities_steps.rb
@@ -8,6 +8,11 @@ When(/^I visit Warrior's page$/) do
   visit character_path(@character)
 end
 
+When(/^I fill in new ability form$/) do
+    fill_in("Name", :with => "speed")
+    fill_in("Level", :with => "23")
+end
+
 When(/^I change ability name to Strength$/) do
   fill_in("Name", :with => "Strength")
 end
--- features/step_definitions/new_ability_steps.rb
+++
@@ -1,4 +0,0 @@
-When(/^I fill in new ability form$/) do
-  fill_in("Name", :with => "speed")
-  fill_in("Level", :with => "23")
-end

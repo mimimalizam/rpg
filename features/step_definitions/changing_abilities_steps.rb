99dfe5e0a31c67179e06653e19b3dee2cc11dd25
Milana Stojadinov <mstojadinov@renderedtext.com>
2015-08-12 13:33:13 +0200

    Removed logged_user feature

--- features/step_definitions/changing_abilities_steps.rb
+++ features/step_definitions/changing_abilities_steps.rb
@@ -1,8 +1,3 @@
-Given(/^there is a character named Warrior$/) do
-  user = User.find_by_username("John")
-  @character = user.characters.create!({ :name => "Warrior" })
-end
-
 Given(/^character Warrior has ability speed$/) do
   character = Character.find_by_name("Warrior")
   @ability = character.abilities.create!({ :name => "speed", :level => "23" })
@@ -15,8 +10,4 @@ end
 
 When(/^I change ability name to Strength$/) do
   fill_in("Name", :with => "Strength")
-end
-
-When(/^I click on "(.*?)" button$/) do |button|  
-  click_button(button)
 end

df7f86a1416c61179eaa629e7c8b1e5e04544f47
Milana Stojadinov <mstojadinov@renderedtext.com>
2015-08-19 16:35:59 +0200

    Successfull character create spec

--- spec/controllers/characters_controller_spec.rb
+++ spec/controllers/characters_controller_spec.rb
@@ -3,30 +3,30 @@ require "rails_helper"
 RSpec.describe CharactersController, :type => :controller do
 
   before do
-    @user =  double(User)
-    allow(controller).to receive(:current_user) { @user }
+    @user = double(User)
     allow(controller).to receive(:authenticate_user!)
   end
   
   describe "POST create" do
-
     context "successfull new character" do
+
       before do
+        @character =  double(Character)
+        allow(@character).to receive(:class) { Character }
         allow(controller).to receive(:current_user) { @user }
-        @character = FactoryGirl.build_stubbed(:character)
         allow(@user).to receive_message_chain(:characters, :build) { @character }
-        allow(@character).to receive(:save) { true}
+        allow(@character).to receive(:save) { true }
       end
 
       it "calls save on character" do
         expect(@character).to receive(:save) { true }
-        post :create , { :character => { :name => "Warrior" } }
+        post :create, { :character => { :name => "Warrior" } }
       end
 
-      #it "should redirect to char types" do
-      # expect(response).to redirect_to(@character)
-      #end
+      it "redirects to the created Character's page" do 
+         post :create, { :character => { :name => "Warrior" } }
+         expect(response).to have_http_status(:redirect)
+      end
     end
   end
 end
-

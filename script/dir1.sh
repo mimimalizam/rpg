4fc1e3f5951b7e5931a8492366723636f776dcf2
Milana <milana.stojadinov@gmail.com>
2017-01-11 17:44:27 +0100

    Change scripts

--- script/dir1.sh
+++ script/dir1.sh
@@ -4,11 +4,13 @@ set -e
 
 cd $SEMAPHORE_PROJECT_DIR/dir1
 
+echo "----------------------------------------------------"
 echo "Installing dependencies of dir1"
 echo "----------------------------------------------------"
 bundle install
 bundle exec rake db:setup db:test:prepare
 
+echo "----------------------------------------------------"
 echo "Running tests for dir1"
 echo "----------------------------------------------------"
 bundle exec rake rspec
--- script/dir2.sh
+++ script/dir2.sh
@@ -4,11 +4,13 @@ set -e
 
 cd $SEMAPHORE_PROJECT_DIR/dir2
 
+echo "----------------------------------------------------"
 echo "Installing dependencies of dir2"
 echo "----------------------------------------------------"
 bundle install
 bundle exec rake db:setup db:test:prepare
 
+echo "----------------------------------------------------"
 echo "Running tests for dir2"
 echo "----------------------------------------------------"
 bundle exec rake rspec
